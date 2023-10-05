c419708882db516512dae42e80faaa1d6a7343f1
thevithach <vitt@uia.no>
2023-10-05 16:02:15 +0200

    Fikset mappestruktur og la til en tydeligere hjemside for bedre navigasjon

---
+++ WebapplikasjonSemesterOppgave/Models/HomeEntity.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace WebapplikasjonSemesterOppgave.Models
+{
+	public class HomeEntity
+	{
+        //Parametre for HomeController for å definere SVG Ikon
+        public string SvgPath { get; set; }
+        public string Text { get; set; }
+    }
+}
+
